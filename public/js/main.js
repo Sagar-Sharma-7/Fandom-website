48113f62235b3357dd22ad583c132eaa91aa220c
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-08-23 12:43:26 +0530

    removed js

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-const span1 = document.querySelector("#span1")
-
-const blink_speed = 1000;
-span1.textContent = "Pokemons"
-
-const blink = setInterval(() => {
-    span1.style.color = (span1.style.color == 'black' ? '' : 'black');
-}, blink_speed);
