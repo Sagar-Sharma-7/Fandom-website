6391c5a6043501bee1f5e52b0b1b1ce43e8d61d7
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2021-08-26 10:35:05 +0530

    shop now button js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
+const buy_btn = document.querySelector("#buy_btn");
 const btn = $('#button');
 
+// shop button
+buy_btn.addEventListener("click", () => {
+  window.open("https://sagar-sharma-7.github.io/Fandom-website/public/Pages/shop.html", "_self")
+})
 
+
+// back to top
 $(window).scroll(function() {
   if ($(window).scrollTop() > 300) {
     btn.addClass('show');
